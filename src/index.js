459c6bd2c74f67472ea2a6dfed88626739a987e7
Saori <saorisabeltovar@gmail.com>
2026-01-13 08:06:56 -0500

    New Beginning

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import renderProducts from './components/fetch-data.js';
-import infiniteScroll from './components/infinite-scroll.js'
-const $observe = document.getElementById('observe');
-window.addEventListener('load', renderProducts);
-window.addEventListener('scroll', infiniteScroll, {passive: false});
